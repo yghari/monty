743ff3173e7d7860b57c365e26c5cbc2996c66fd
Oumaelba <elbachirioumaima16@gmail.com>
2023-06-22 10:18:25 +0100

    mod del

--- calcul.c
+++ calcul.c
@@ -86,32 +86,3 @@ void mul_function(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n *= (*stack)->n;
 	pop_function(stack, line_number);
 }
-
-/**
- * mod_function - Computes the remainder of the division of the second top element
- *                of the stack by the top element.
- * @stack: Pointer to the head of the stack.
- * @line_number: Current line number of the bytecode file.
- *
- * Return: void
- */
-void mod_function(stack_t **stack, unsigned int line_number)
-{
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
-		free_td(*stack);
-		exit(EXIT_FAILURE);
-	}
-
-	if ((*stack)->n == 0)
-	{
-		fprintf(stderr, "L%u: division by zero\n", line_number);
-		free_td(*stack);
-		exit(EXIT_FAILURE);
-	}
-
-	int result = (*stack)->next->n % (*stack)->n;
-	pop_function(stack, line_number);
-	(*stack)->n = result;
-}
