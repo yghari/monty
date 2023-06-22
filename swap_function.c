d808f604f7160da6746f9a2643d56f3660192918
Oumaelba <elbachirioumaima16@gmail.com>
2023-06-22 08:48:31 +0100

    update swap

--- swap_function.c
+++ swap_function.c
@@ -20,7 +20,7 @@ void swap_function(stack_t **h, unsigned int c)
 	if (len < 2)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", c);
-		fclose(data.file_descriptor);
+		/*fclose(data.file_descriptor);*/
 		free(data.buff);
 		free_td(*h);
 		exit(EXIT_FAILURE);
